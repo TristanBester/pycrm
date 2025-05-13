f0ab92d0d88049dfda5c82460154a89921d8b61c
TristanBester <tristanbester@gmail.com>
2025-05-13 19:27:24 +0200

    added introduction example

---
+++ examples/introduction/core/__init__.py
@@ -0,0 +1,11 @@
+from examples.introduction.core.crossproduct import LetterWorldCrossProduct
+from examples.introduction.core.ground import LetterWorld
+from examples.introduction.core.label import LetterWorldLabellingFunction
+from examples.introduction.core.machine import LetterWorldCountingRewardMachine
+
+__all__ = [
+    "LetterWorldCrossProduct",
+    "LetterWorld",
+    "LetterWorldLabellingFunction",
+    "LetterWorldCountingRewardMachine",
+]
