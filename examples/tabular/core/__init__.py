7165dae2e6cc4531096759bdcddb5aad74dab4b9
TristanBester <tristanbester@gmail.com>
2025-05-13 19:28:10 +0200

    added tabular example

---
+++ examples/tabular/core/__init__.py
@@ -0,0 +1,11 @@
+from examples.tabular.core.crossproduct import OfficeWorldCrossProduct
+from examples.tabular.core.ground import OfficeWorld
+from examples.tabular.core.label import OfficeWorldLabellingFunction
+from examples.tabular.core.machine import OfficeWorldCountingRewardMachine
+
+__all__ = [
+    "OfficeWorldCrossProduct",
+    "OfficeWorld",
+    "OfficeWorldLabellingFunction",
+    "OfficeWorldCountingRewardMachine",
+]
