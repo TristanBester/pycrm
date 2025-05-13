c53d91e5862e9524637f8a3139206f4fd1307120
TristanBester <tristanbester@gmail.com>
2025-05-13 19:32:47 +0200

    added partial implementation of discrete example

---
+++ examples/discrete/core/__init__.py
@@ -0,0 +1,11 @@
+from examples.discrete.core.crossproduct import PuckWorldCrossProduct
+from examples.discrete.core.ground import PuckWorld
+from examples.discrete.core.label import PuckWorldLabellingFunction
+from examples.discrete.core.machine import PuckWorldCountingRewardMachine
+
+__all__ = [
+    "PuckWorldCountingRewardMachine",
+    "PuckWorldCrossProduct",
+    "PuckWorldLabellingFunction",
+    "PuckWorld",
+]
